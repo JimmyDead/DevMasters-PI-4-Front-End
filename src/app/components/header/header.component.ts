266e1c0b5e0f1f46df9abf5c1099f679f243858a
marcos.viniciusk8@hotmail.com <marcos.viniciusk8@hotmail.com>
2021-02-28 17:36:28 -0300

    carrinho no header recebendo informacoes dos produtos adicionados na home, simplificando, agora quando adiciono um produto ele aparece no carrinho com suas respectivas informacoes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,3 +1,5 @@
+import { CartService } from './../../services/cart.service';
+import { CartModelServer } from './../../model/cart.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  carrinhoData: CartModelServer
+  carrinhoTotal: number
+
+  constructor(public cartService: CartService) { }
 
   ngOnInit(): void {
+    //pegando informacoes da classe cartService na inicializacao
+    this.cartService.totalCarrinho.subscribe(total => this.carrinhoTotal = total)
+    this.cartService.dataCarrinho.subscribe(data => this.carrinhoData = data)
+  }
+
+  //abre o dialogo perguntando se deseja excluir o produto
+  openDialogDeleteProduct(index, produto) {
+    this.cartService.openDialogDeleteProduct(index, produto)
   }
 
 }
